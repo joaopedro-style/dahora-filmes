e4a43f49b0950f747287c9e0cd67e8e8fb2aaf75
joaopedro-style <joaopedro18231@hotmail.com>
2025-09-16 11:43:06 -0300

    Adiciona StyleSheet.create

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,13 @@
-import { ActivityIndicator, View } from "react-native";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 
 export default function Loading() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={estilos.loading}>
       <ActivityIndicator size={100} />
     </View>
   );
 }
+
+const estilos = StyleSheet.create({
+  loading: { flex: 1, justifyContent: "center", alignItems: "center" },
+});
