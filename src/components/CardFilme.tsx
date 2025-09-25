2852709e20ee4d1fe44e7a2ac4424e8bf44ebaa3
joaopedro-style <joaopedro18231@hotmail.com>
2025-09-25 11:59:50 -0300

    Adiciona pthname

--- src/components/CardFilme.tsx
+++ src/components/CardFilme.tsx
@@ -15,7 +15,8 @@ export default function CardFilme({ filme }: CardFilmeProps) {
 
   const leiaMais = () => {
     router.push({
-      pathname: "/detalhes/[id]",
+      // pathname: "/detalhes/[id]",
+      pathname: `/detalhes/${id}`,
     });
   };
 
