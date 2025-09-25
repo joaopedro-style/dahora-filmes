60d432ea647569bed61deca7f8e1a3fcb5dc28b8
joaopedro-style <joaopedro18231@hotmail.com>
2025-09-25 09:42:40 -0300

    testa carregamento de filmes com map

--- app/resultados.tsx
+++ app/resultados.tsx
@@ -52,7 +52,13 @@ export default function Resultados() {
           Você buscou por: <Text style={estilos.termo}> {filme} </Text>
         </Text>
 
-        {loading ? <Loading /> : <Text>Busca finalizada!</Text>}
+        {loading ? (
+          <Loading />
+        ) : (
+          resultados.map((resultado) => (
+            <Text key={resultado.id}>{resultado.title}</Text>
+          ))
+        )}
       </SafeAreaView>
     </>
   );
