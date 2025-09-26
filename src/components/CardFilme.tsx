508c17d14e9efae7b4a42b6d6326e0ec80e18919
joaopedro-style <joaopedro18231@hotmail.com>
2025-09-26 08:43:09 -0300

    Adiciona params e JSON.stringify

--- src/components/CardFilme.tsx
+++ src/components/CardFilme.tsx
@@ -16,6 +16,11 @@ export default function CardFilme({ filme }: CardFilmeProps) {
   const leiaMais = () => {
     router.push({
       pathname: "/detalhes/[id]",
+      params: {
+        // Convertemos o filme completo para string/json
+        // enviamos como parâmetro para a rota de detalhes
+        filme: JSON.stringify(filme),
+      },
     });
   };
 
